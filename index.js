b243622b803795330bc7d251a61f0f6aeb22738c
Akhil Murali <github@akhilmurali.com>
2018-09-06 21:04:56 +0530

    Fix port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let app = express();
-let port = process.env.port || 4000;
+let port = process.env.PORT || 4000;
 
 app.get('/', (req, res)=>{
     res.json({msg: 'Hi from remote server'});
 });
 
 app.listen(port, ()=>{
-    console.log('App listening on port: ' + process.env.port);
+    console.log('App listening on port: ' + port);
 });
