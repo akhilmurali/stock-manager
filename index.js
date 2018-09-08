6ea62d0d952f3839cde6e0916f94e1e5d9ff3739
Akhil Murali <github@akhilmurali.com>
2018-09-08 09:10:34 +0530

    Bug fix

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ let express = require('express');
 let cors = require('cors');
 let app = express();
 let port = process.env.PORT || 4000;
+let bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res)=>{
+    console.log(req.url);
     res.json({msg: 'Hi from remote server'});
 });
 
 app.post('/image', (req, res)=>{
-    res.status({status: 'ok'});
+    console.log(req.body);
+    res.json({status: 'ok'});
 });
 
 app.get('/image/latest', (req, res)=>{
-    console.log(req.body);
-    res.status({status: 'ok'});
+    console.log(req.url);
+    res.json({status: 'ok'});
 });
 
 app.listen(port, ()=>{
