da2743d878c1cb7146d1c8fdf1ca70ceffc68222
Akhil Murali <github@akhilmurali.com>
2018-09-08 21:00:54 +0530

    Add python code to capture snaps

--- RaspberryPi/camera.py
+++ RaspberryPi/camera.py
@@ -2,15 +2,16 @@ from picamera import PiCamera
 import requests
 import time 
 
-camera = PiCamera()
-camera.preview_fullscreen=False
-camera.preview_window=(620, 320, 640, 480)
-camera.resolution=(640,480)
-camera.start_preview()
-time.sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
-camera.stop_preview()
-d = requests.get(url = 'https://safe-cliffs-18013.herokuapp.com')
-print(d.encoding)
-print(d.text)
-print(d.json())
+while True:
+    camera = PiCamera()
+    camera.preview_fullscreen=False
+    camera.preview_window=(620, 320, 640, 480)
+    camera.resolution=(640,480)
+    camera.start_preview()
+    time.sleep(5)
+    camera.capture('/home/pi/Desktop/image.jpg')
+    camera.stop_preview()
+    with open('/home/pi/Desktop/image.jpg', 'rb') as f:
+        r = requests.post('https://safe-cliffs-18013.herokuapp.com/image', files={'fridge.jpg': f})
+    #Take snaps once in every 30 mins:
+    time.sleep(1800)
